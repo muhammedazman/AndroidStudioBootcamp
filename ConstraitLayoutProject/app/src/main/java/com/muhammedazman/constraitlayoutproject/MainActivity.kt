cb69c777368ac7cfd20bb6512c17c581bcb00280
DESKTOP-J343IEJ\akyus <vmazman@gmail.com>
2021-02-27 22:20:48 +0100

    MainActivity.kt Toast kodu yazildi

--- ConstraitLayoutProject/app/src/main/java/com/muhammedazman/constraitlayoutproject/MainActivity.kt
+++ ConstraitLayoutProject/app/src/main/java/com/muhammedazman/constraitlayoutproject/MainActivity.kt
@@ -2,10 +2,21 @@ package com.muhammedazman.constraitlayoutproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textInputEditTextUsername: TextInputEditText = findViewById(R.id.textInputEditTextUsername)
+        val textInputEditTextPassword: TextInputEditText = findViewById(R.id.textInputEditTextPassword)
+        val buttonLogin: MaterialButton = findViewById(R.id.buttonLogin)
+
+        buttonLogin.setOnClickListener {
+            Toast.makeText(this, "${textInputEditTextUsername.text} - ${textInputEditTextPassword.text}", Toast.LENGTH_LONG).show();
+        }
     }
 }
